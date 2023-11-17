d791406aaa18f9c843e0b7e7c95fc6d5ded0a9d1
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-17 12:35:16 +0530

    reading and writing from a file

--- Backend/createServer.js
+++ Backend/createServer.js
@@ -1,26 +1,46 @@
 const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer( (req, res) => {
     const url = req.url
-    if(url=='/'){
+    if (url == '/') {
+        let a = fs.readFileSync('message.txt').toString()
+        console.log(a,'222')
         res.write('<html>')
         res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Hello from my NodeJs Server</h1></body>')
+        res.write('<body>')
+        res.write(`<p id="msgpara">${a}</p>`)
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>')
+        res.write('</body>')
         res.write('</html>')
         return res.end();
-    }else if(url=='/home'){
+    } else if (url == '/message' && req.method == 'POST') {
+        const data = []
+        req.on('data', (chunk) => {
+            data.push(chunk)
+        })
+
+        req.on('end', () => {
+            const parsedData = Buffer.concat(data).toString()
+            const msg = parsedData.split('=')[1]
+            fs.writeFileSync('message.txt', msg)
+        })
+        res.statusCode = 302
+        res.setHeader('location', '/')
+        return res.end();
+    } else if (url == '/home') {
         res.write('<html>')
         res.write('<head><title>My First Page</title></head>')
         res.write('<body><h1>Welcome Home</h1></body>')
         res.write('</html>')
         return res.end();
-    }else if(url=='/about'){
+    } else if (url == '/about') {
         res.write('<html>')
         res.write('<head><title>My First Page</title></head>')
         res.write('<body><h1>Welcome to about us page</h1></body>')
         res.write('</html>')
         return res.end();
-    }else if(url=='/node'){
+    } else if (url == '/node') {
         res.write('<html>')
         res.write('<head><title>My First Page</title></head>')
         res.write('<body><h1>Welcome to my NodeJs Project</h1></body>')
