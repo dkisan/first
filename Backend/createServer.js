95c95a77f59105ae199d461965c900a875352c23
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-17 23:19:12 +0530

    middleware bodyParser

--- Backend/createServer.js
+++ Backend/createServer.js
@@ -1,16 +1,24 @@
 const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 // const handler = require('./routes')
 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('I am in the middleware')
-    next()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/addproduct', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="prodName"><input type="number" name="prodSize"><button type="submit">Add Product</button></form>')
+})
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/addproduct')
 })
 
-app.use((req,res,next)=>{
-    console.log("I am in another middleware")
+
+
+app.use('/', (req, res, next) => {
+    // console.log("I am in another middleware")
     res.send('<h1>Hello from Express Js</h1>')
 })
 
