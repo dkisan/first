7e662e2977c677d0d8dc67886b044cd6d74999f9
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-17 19:20:18 +0530

    clean code

--- Backend/createServer.js
+++ Backend/createServer.js
@@ -1,52 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer( (req, res) => {
-    const url = req.url
-    if (url == '/') {
-        let a = fs.readFileSync('message.txt').toString()
-        console.log(a,'222')
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body>')
-        res.write(`<p id="msgpara">${a}</p>`)
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>')
-        res.write('</body>')
-        res.write('</html>')
-        return res.end();
-    } else if (url == '/message' && req.method == 'POST') {
-        const data = []
-        req.on('data', (chunk) => {
-            data.push(chunk)
-        })
+const handler = require('./routes')
 
-        req.on('end', () => {
-            const parsedData = Buffer.concat(data).toString()
-            const msg = parsedData.split('=')[1]
-            fs.writeFileSync('message.txt', msg)
-        })
-        res.statusCode = 302
-        res.setHeader('location', '/')
-        return res.end();
-    } else if (url == '/home') {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Welcome Home</h1></body>')
-        res.write('</html>')
-        return res.end();
-    } else if (url == '/about') {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Welcome to about us page</h1></body>')
-        res.write('</html>')
-        return res.end();
-    } else if (url == '/node') {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Welcome to my NodeJs Project</h1></body>')
-        res.write('</html>')
-        return res.end();
-    }
-})
+const server = http.createServer(handler)
 
 server.listen(4000)
---
+++ Backend/routes.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+function reqHandler(req, res) {
+    const url = req.url
+    if (url == '/') {
+        let a = fs.readFileSync('message.txt').toString()
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body>')
+        res.write(`<p id="msgpara">${a}</p>`)
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    } else if (url == '/message' && req.method == 'POST') {
+        const data = []
+        req.on('data', (chunk) => {
+            data.push(chunk)
+        })
+
+        req.on('end', () => {
+            const parsedData = Buffer.concat(data).toString()
+            const msg = parsedData.split('=')[1]
+            fs.writeFileSync('message.txt', msg)
+        })
+        res.statusCode = 302
+        res.setHeader('location', '/')
+        return res.end();
+    } else if (url == '/home') {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        return res.end();
+    } else if (url == '/about') {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    } else if (url == '/node') {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to my NodeJs Project</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+}
+
+module.exports = reqHandler;
+
+// exports.handler = reqHandler;
+// exports.msg = 'Some hard coded text'
+
+// module.exports = {
+//     handler: reqHandler,
+//     msg: 'Some hard coded text'
+// }
