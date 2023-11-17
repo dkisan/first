a9b8b1eefaaa5030913cf20ecc29874298fdfa98
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-17 05:48:34 +0530

    map

--- Backend/helloworld.js
+++ Backend/helloworld.js
@@ -1,14 +1,24 @@
 // console.log('Hello World !!!')
 
-const add = (a, b) => a + b
-console.log(add(2, 3))
+// const add = (a, b) => a + b
+// console.log(add(2, 3))
 
-const student = {
-    firstname: "Debasish",
-    lastname: "Kisan",
-    rollno: 1,
-    printfullname: function () {
-        console.log('Hi my name is ' + this.firstname + " " + this.lastname)
+// const student = {
+//     firstname: "Debasish",
+//     lastname: "Kisan",
+//     rollno: 1,
+//     printfullname: function () {
+//         console.log('Hi my name is ' + this.firstname + " " + this.lastname)
+//     }
+// };
+// student.printfullname()
+
+const arr = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+const n_arr = arr.map(e=>{
+    if(e==' '){
+        return 'empty string'
+    }else{
+        return e
     }
-};
-student.printfullname()
+})
+console.log(n_arr)
